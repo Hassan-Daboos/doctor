df9e97bc21f74e0dd5d4ba39679b26fbac4cf51c
Hassan Daboos <62604224+Hassan-Daboos@users.noreply.github.com>
2023-04-10 00:17:02 +0200

    update add picture

--- lib/view/screens/layouthome/profileScreens/addMedicalHistory.dart
+++ lib/view/screens/layouthome/profileScreens/addMedicalHistory.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:doctor/constant/color_manager.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../component/app_component/custom_text.dart';
 
@@ -12,6 +17,109 @@ class addMedicalHistory extends StatefulWidget {
 }
 
 class _addMedicalHistoryState extends State<addMedicalHistory> {
+  File? imageFile;
+  var base64Imageteam;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Future<void> _showSelectionDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text("From where do you want to take the photo?"),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    GestureDetector(
+                      child: Text("Gallery"),
+                      onTap: () {
+                        _getFromGallery();
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    GestureDetector(
+                      child: Text("Camera"),
+                      onTap: () {
+                        _getFromCamera();
+                        Navigator.pop(context);
+                      },
+                    ),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    GestureDetector(
+                      child: Text("File"),
+                      onTap: () {
+                        _getFromFile();
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                ),
+              ));
+        });
+  }
+
+  /// Get from gallery
+  _getFromGallery() async {
+    PickedFile? pickedFile = await ImagePicker().getImage(
+      source: ImageSource.gallery,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+        print("imageFile----------------------------->$imageFile");
+
+        List<int> imageBytes = imageFile!.readAsBytesSync();
+        base64Imageteam = base64Encode(imageBytes);
+        print(base64Imageteam);
+        // updatimge(base64Image, id);
+      });
+    }
+  }
+
+  /// Get from File
+  _getFromFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+    if (result != null) {
+      imageFile = File(result.files.single.path!);
+
+      print("File----------------------------->$imageFile");
+
+      List<int> fileBytes = imageFile!.readAsBytesSync();
+      base64Imageteam = base64Encode(fileBytes);
+      print(base64Imageteam);
+      print(imageFile!.path);
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  /// Get from Camera
+  _getFromCamera() async {
+    PickedFile? pickedFile = await ImagePicker().getImage(
+      source: ImageSource.camera,
+      maxWidth: 1800,
+      maxHeight: 1800,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        imageFile = File(pickedFile.path);
+        // print("imageFile----------------------------->$imageFile");
+        // List<int> imageBytes = imageFile.readAsBytesSync();
+        // print("dddd$imageBytes");
+        // base64Image = base64UrlEncode(imageBytes);
+        List<int> imageBytes = imageFile!.readAsBytesSync();
+        base64Imageteam = base64Encode(imageBytes);
+        print(base64Imageteam);
+
+        // updatimge(base64Image, id);
+      });
+    }
+  }
+
   var dateTime;
 
   TextEditingController dateTimeController = TextEditingController();
@@ -26,23 +134,24 @@ class _addMedicalHistoryState extends State<addMedicalHistory> {
       appBar: AppBar(
         elevation: 0,
         centerTitle: true,
-
         backgroundColor: Colors.transparent,
-        title:  CustomText(
+        title: CustomText(
           text: 'Add Medical History',
           fontSize: 25,
           color: textcolor,
           fontWeight: FontWeight.bold,
         ),
-actions: [IconButton(
-    onPressed: () {
-      Navigator.of(context).pop();
-    },
-    icon: Icon(
-      Icons.save_alt,
-      color: textcolor,
-    )),],
-        leading:  IconButton(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              icon: Icon(
+                Icons.save_alt,
+                color: textcolor,
+              )),
+        ],
+        leading: IconButton(
             onPressed: () {
               Navigator.of(context).pop();
             },
@@ -50,7 +159,6 @@ actions: [IconButton(
               Icons.arrow_back_ios,
               color: textcolor,
             )),
-
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -124,8 +232,6 @@ actions: [IconButton(
                   style: TextStyle(color: textcolor),
                 ),
               ),
-
-
             ],
           ),
         ),
